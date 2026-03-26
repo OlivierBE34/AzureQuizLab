930524c0506ab2f8c056cee2c94f3e4e5c20e56e
Olivier Beaud <O.BEAUD.ext@fayatit.fayat.com>
2026-03-26 14:40:34 +0100

    maintenanceMode

--- WebApplication1/Pages/Index.cshtml.cs
+++ WebApplication1/Pages/Index.cshtml.cs
@@ -5,16 +5,19 @@ namespace WebApplication1.Pages
 {
     public class IndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
+        public bool MaintenanceMode { get; set; }
 
-        public IndexModel(ILogger<IndexModel> logger)
+        private readonly ILogger<IndexModel> _logger;
+        private readonly IConfiguration _configuration;
+
+        public IndexModel(ILogger<IndexModel> logger, IConfiguration config)
         {
             _logger = logger;
+            _configuration = config;
         }
-
         public void OnGet()
         {
-
+            MaintenanceMode = _configuration.GetValue<bool>("MaintenanceMode", false);
         }
     }
 }
